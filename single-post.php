1dc09a9ddcd7864be708585ebed694427cfcadf8
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-09-22 10:01:11 +0200

    v2.1

--- single-post.php
+++ single-post.php
@@ -3,10 +3,13 @@ get_header();
 ?>
 <h1>Ceci est votre article !!!</h1>
 <?php if (have_posts()):?>
+    <? var_dump(get_field('info')); ?>
 <ul>
 <?php while(have_posts()): the_post(); ?>
 <li><a href="<?php the_permalink()?>"><?php the_title() ?></a></li>
 <div><?php the_content() ?> </div>
+<?php echo get_field('info'); ?>
+
 <?php endwhile ?>
 </ul>
 <?php else: ?>
