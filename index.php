eda8c80ac49f87f2fbda1813c4afc85dff05fc01
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-09-24 12:28:54 +0200

    v2

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php 
 get_header();
 ?>
-Hello world : <?php  wp_title()?>
-<?php if (have_posts()):?>
-<ul>
-<?php while(have_posts()): the_post(); ?>
-<li><a href="<?php the_permalink()?>"><?php the_title() ?></a></li>
-<div><?php the_content() ?> </div>
-<?php endwhile ?>
-</ul>
-<?php else: ?>
-<h1>Pas d'article</h1>
-<?php endif; ?>
-<?php montheme_pagination()?>
+
+<!-- <?php var_dump(get_field('bandeau_image_d')); ?> -->
+<img class="image" src="<?php echo get_field('bandeau_image_g')['url'] ?>">
+<img class="image" src="<?php echo get_field('bandeau_image_d')['url'] ?>">
+
+<?php echo get_field('info'); ?>
+<?php
+if (get_field('bandeau_bouton')==true){
+$link = get_field('bandeau_bouton')['link'];
+echo '<a href="',$link,'">';
+echo '<button type="button">';
+ echo get_field('bandeau_bouton')['nom'];
+ echo '</button>';
+ echo '</a>';
+}
+?>
+
 
 <?php get_footer()?>
--- single-post.php
+++ single-post.php
@@ -8,9 +8,20 @@ get_header();
 <?php while(have_posts()): the_post(); ?>
 <li><a href="<?php the_permalink()?>"><?php the_title() ?></a></li>
 <div><?php the_content() ?> </div>
-<img class="image" src="<?php echo get_field('bandeau_image')['url'] ?>">
+<img class="image" src="<?php echo get_field('bandeau_image_g')['url'] ?>">
+<img class="image" src="<?php echo get_field('bandeau_image_d')['url'] ?>">
+
 <?php echo get_field('info'); ?>
-<?php var_dump(get_field('bouton')); ?>
+<?php
+if (get_field('bandeau_bouton')==true){
+$link = get_field('bandeau_bouton')['link'];
+echo '<a href="',$link,'">';
+echo '<button type="button">';
+ echo get_field('bandeau_bouton')['nom'];
+ echo '</button>';
+ echo '</a>';
+}
+?>
 <?php endwhile ?>
 </ul>
 <?php else: ?>
